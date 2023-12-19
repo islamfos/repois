a91b566d4833a7ccaedfbc554871a08e05944088
Islam <islamaouf335@gmail.com>
2023-12-19 04:01:04 +0200

    simple calculator

--- ver1.cpp
+++ ver1.cpp
@@ -1,1 +1,16 @@
 cout << "hello";
+int sum(int a, int b) {
+	int total = a + b;
+	return total;
+}
+int diff(int a, int b) {
+	int dif = a - b;
+	return dif;
+}
+int multi(int a, int b) {
+	int product = a * b;
+	return product;
+}
+int x = sum(1, 2);
+int y = diff(1, 2);
+int z = multi(1, 2);
